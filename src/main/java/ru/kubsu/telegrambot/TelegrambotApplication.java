2f9d275121c208bd4339f69d60d4ab9e5baed344
Arina Usacheva <usacheva.arina03@mail.ru>
2025-03-21 18:45:57 +0300

    Application yml and Controller

--- src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
+++ src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
@@ -2,11 +2,15 @@ package ru.kubsu.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.kubsu.telegrambot.controller.TestController;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
 	public static void main(String[] args) {
+		TestController controller = new TestController();
+		String hello = controller.hello();
+        System.out.println(hello);
 
 		SpringApplication.run(TelegrambotApplication.class, args);
 	}
